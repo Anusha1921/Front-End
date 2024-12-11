2ba1261bdbbea132d6a10f4bd58b070bcdaf6abd
Anusha1921 <anusha.s1921@gmail.com>
2024-12-11 14:53:55 +0530

    fetch posts

---
+++ Day15/index1.js
@@ -0,0 +1,8 @@
+let car={
+    brand:"Anu",
+    model:"2000",
+    year:2024
+  };
+  console.log(car.brand)
+  console.log(car.model)
+  console.log(car.year)
---
+++ Day15/index2.js
@@ -0,0 +1,7 @@
+let person={};
+person.firstName="Anusha";
+person.lastName="Suryadevara";
+person.age=22;
+
+delete person.age;
+console.log(person)
